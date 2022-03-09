f0ceb7824e00bc6b4c5d357115508b3d3284742d
Brodie <brodiesnelling@gmail.com>
2022-03-09 10:08:18 -0500

    fixed files

--- index.js
+++ index.js
@@ -7,14 +7,11 @@ const CORS = require('cors');
 const app = express();
 const PORT = process.env.PORT;
 
-
 // middleware
 app.use(express.json());
 app.use(express.static('public'));
 app.use(CORS());
 app.use(videoRoutes);
-
-
 
 app.listen(PORT, () => {
     console.log(`Hello! My server is listening on ${PORT}`);
