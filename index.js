41986b942eade9c2f0e1cccdbfcc2827434eee3b
Brodie <brodiesnelling@gmail.com>
2022-03-09 11:38:48 -0500

    updated server code

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ require('dotenv').config();
 
 const videoRoutes = require('./routes/videos');
 
-const CORS = require('cors');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT;
 
 // middleware
 app.use(express.json());
 app.use(express.static('public'));
-app.use(CORS());
+app.use(cors());
 app.use(videoRoutes);
 
 app.listen(PORT, () => {
--- routes/videos.js
+++ routes/videos.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const router = express.Router();
-// const fs = require('fs');
+const fs = require('fs');
+const PORT = process.env.PORT;
+const URL = process.env.URL;
 
-const videosData = '../data/videos.json';
-const videos = require(videosData);
 
-
-//Get /videos that will respond with the array of videos
+//Get all videos that will respond with the array of videos
 router.get('/', (req, res) => {
-    res.send('hello worldsssssssss');
+    fs.readFile('./data/videos.json', 'utf8', (err, data) => {
+        console.log(data);
+        if (err) {
+            res.send('error reading comments data');
+          } else {
+            const videoData = JSON.parse(data);
+            const videoList = videoData.map(video => {
+                return {
+                    id: video.id,
+                    title: video.title,
+                    channel: video.channel,
+                    image: `${URL}:${PORT}/images/${video.image}`
+                }
+            });
+            res.json(videoList);
+            console.log('working');
+        }
+    })  
 });
 
 router.get('/videos', (req, res) => {
@@ -17,6 +33,10 @@ router.get('/videos', (req, res) => {
 
 module.exports = router;
 //get /videos/:id that responds with object containing the details based on video id
+
+
+
+
 
 
 //post to post new videos to server
